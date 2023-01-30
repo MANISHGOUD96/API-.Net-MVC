2c2fa9b3ad55a908f8477745b35441f88a01913b
Manish Goud <76445347+MANISHGOUD96@users.noreply.github.com>
2023-01-30 15:19:35 +0530

    API MVC

---
+++ EmpModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MK_API_WebMVC.Models
+{
+    public class EmpModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double? Age { get; set; }
+        public string Adress { get; set; }
+        public string Company { get; set; }
+        public double? Salary { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Addhar { get; set; }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,87 @@
+﻿using MK_API_WebMVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MK_API_WebMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //---------------Read the table Data---------------------
+        public ActionResult Index()
+        {
+            
+          //...........1st step...........
+            HttpClient client = new HttpClient();
+
+            //...........2nd step...........
+            var data =client.GetAsync("http://localhost:62369/Api/GetEMPData").Result;
+
+            //...........3rd step...........
+            var readdata = data.Content.ReadAsStringAsync().Result;
+
+            //.....4th Convert JSON to Class.......... 
+            var empres=JsonConvert.DeserializeObject<List<EmpModel>>(readdata);
+
+            return View(empres);
+        }
+
+        //---------------Add Data in Table---------------------
+
+        [HttpGet]
+        public ActionResult AddEmp()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddEmp(EmpModel obj)
+        {
+            //...........1st step...........
+            HttpClient client = new HttpClient();
+            var data = JsonConvert.SerializeObject(obj);
+            StringContent postdata = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+            var result = client.PostAsync("http://localhost:62369/Api/SaveData", postdata).Result;
+            return RedirectToAction("Index");
+        }
+
+        //---------------Delete Data from Table---------------------
+        public ActionResult Delete(int Id)
+        {
+            HttpClient client = new HttpClient();
+            var result = client.GetAsync("http://localhost:62369/Api/DeleteData" + "?Id=" + Id).Result;
+            return RedirectToAction("Index");
+        }
+
+        //---------------Edit Data from Table---------------------
+        public ActionResult Edit(int Id)
+        {
+            HttpClient client = new HttpClient();
+            var result = client.GetAsync("http://localhost:62369/Api/EditData" + "?Id=" + Id).Result;
+
+            var data = result.Content.ReadAsStringAsync().Result;
+            var objdata = JsonConvert.DeserializeObject<EmpModel>(data);
+            return View("AddEmp", objdata);
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
